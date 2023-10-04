2bdf08e4cd9506c34cb0d2d9d652b6d0284eaef5
jvictorsar <joao.guazzelli@engdb.com.br>
2023-10-04 12:35:00 -0300

    feat: add initial guariglia

---
+++ guariglia/guariglia.py
@@ -0,0 +1,44 @@
+import math
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+import utils
+
+url = "https://www.guariglialeiloes.com.br/"
+
+response = requests.get(url)
+if response.status_code == 200:
+    html = response.text
+    soup = bs(html, "lxml")
+    tables = soup.find_all('div', {'class': 'card-body d-flex flex-column'})
+    links = []
+    for item in tables:
+        link = {'link': item.find('a')['href']}
+        if item.find('div', {'class': 'label_leilao rounded ativo'}):
+            link['status'] = item.find('div', {'class': 'label_leilao rounded ativo'}).text
+            aux = item.find_all('div', text=lambda text: text and 'Lotes' in text)
+            link['auctions'] = int(aux[0].text[:3])
+        links.append(link)
+
+    main_link = []
+    for link in links:
+        try:
+            if link['status']:
+                main_link.append(link)
+        except KeyError:
+            pass
+    pages = math.ceil(main_link[0]['auctions'] / 30)
+    page_link = main_link[0]['link'] + "?page="
+
+    items = []
+
+    for page in range(1, pages):
+        response = requests.get(page_link+str(page))
+        html = response.text
+        soup = bs(html, "lxml")
+        lots = soup.find_all('div', {'class': 'lote rounded'})
+        items.append(utils.build_item(lots))
+
+    utils.save_csv(items)
---
+++ guariglia/utils.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def build_item(lots):
+    items = []
+    for lot in lots:
+        item = {}
+
+        text = find_attribute('Marca/Modelo', lot)
+
+        manufacturer, model = get_model_and_manufacturer(text)
+        item['manufacturer'] = manufacturer
+        item['model'] = model
+
+        text = find_attribute('Ano/Modelo', lot)
+        item['year'] = text
+
+        text = find_attribute('Combustível', lot)
+        item['gas'] = text
+
+        text = find_attribute('KM', lot)
+        item['km'] = text
+
+        text = find_attribute('Despesas', lot)
+        item['expenses'] = text
+
+        text = find_last_bid(lot)
+        item['last_bid'] = text
+
+        items.append(item)
+
+    return items
+
+
+def find_attribute(keyword, lot):
+    aux = lot.find('b', text=lambda text: text and keyword in text)
+    if aux:
+        text = aux.find_next_sibling(text=True)
+        if text:
+            return text
+
+
+def find_last_bid(lot):
+    text = lot.find('div', {'class': 'lance_atual'})
+    if text:
+        return text.text
+
+
+def get_model_and_manufacturer(text):
+    try:
+        aux = text.split('/')
+        manufacturer = aux[0]
+        model = aux[1]
+        return manufacturer, model
+    except:
+        return '', ''
+def save_csv(items):
+    df = pd.DataFrame(items)
+    df.to_csv('guariglia_result.csv', index=False)
